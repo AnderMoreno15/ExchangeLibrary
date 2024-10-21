55985c895031044c3b26d7b26aaccbd8c51479b8
InigoFreire <inifreire@gmail.com>
2024-10-21 10:38:29 +0200

    Updated User class

--- src/userLogicTier/model/User.java
+++ src/userLogicTier/model/User.java
@@ -5,102 +5,50 @@
  */
 package userLogicTier.model;
 
-import userLogicTier.Signable;
-
 /**
  *
  * @author inifr
  */
-public class User implements Signable {
-    private String login;
+public class User{
+    private String name;
+    private String email;
     private String password;
-    private boolean active;
-    private String city;
     private String street;
-    private int zipCode;
-    
-    public User(String loginInput, String passwordInput, boolean activeInput) {
-        this.login = loginInput;
-        this.password = passwordInput;
-        this.active = activeInput;
-        this.city = "";
+    private String city;
+    private String zip;
+    private boolean active;
+    private int company_id;
+    private String notification_type;
+
+    //Constructor for testing purposes
+    public User() {
+        this.name = "";
+        this.email = "";
+        this.password = "";
         this.street = "";
-        this.zipCode = 0;
+        this.city = "";
+        this.zip = "";
+        this.active = false;
+        this.company_id = 1;
+        this.notification_type = "email";
     }
 
-    public User(String loginInput, String passwordInput, boolean activeInput, String cityInput, String streetInput, int zipCodeInput) {
-        this.login = loginInput;
+    //Constructor for SignUp
+    public User(String nameInput, String emailInput, String passwordInput, String streetInput, String cityInput, String zipInput, boolean activeInput, int company_idInput, String notification_typeInput) {
+        this.name = nameInput;
+        this.email = emailInput;
         this.password = passwordInput;
-        this.active = activeInput;
-        this.city = cityInput;
         this.street = streetInput;
-        this.zipCode = zipCodeInput;
+        this.city = cityInput;
+        this.zip = zipInput;
+        this.active = activeInput;
+        this.company_id = company_idInput;
+        this.notification_type = notification_typeInput;
     }
 
-    public String getLogin() {
-        return login;
+    //Constructor for SignIn
+    public User(String emailInput, String passwordInput) {
+        this.email = emailInput;
+        this.password = passwordInput;
     }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public boolean isActive() {
-        return active;
-    }
-
-    public void setActive(boolean active) {
-        this.active = active;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getStreet() {
-        return street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public int getZipCode() {
-        return zipCode;
-    }
-
-    public void setZipCode(int zipCode) {
-        this.zipCode = zipCode;
-    }
-
-    @Override
-    public User signUp(String loginInput, String passwordInput, boolean activeInput, String cityInput,
-            String streetInput, int zipCodeInput) {
-        //TODO meter código del método
-        User user = new User(loginInput, passwordInput, activeInput, cityInput, streetInput, zipCodeInput);
-
-        return user;
-    }
-
-    @Override
-    public User SignIn(String loginInput, String passwordInput, boolean activeInput) {
-        //TODO meter código del método
-        User user = new User(loginInput, passwordInput, activeInput);
-
-        return user;
-    }
-
-    
 }
