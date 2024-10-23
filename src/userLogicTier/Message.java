10fad2f8b7f53a4881f0f86e440a9deec3453b89
pablo <whereispebble@gmail.com>
2024-10-23 12:50:59 +0200

    message and messagetype classes created and updated

---
+++ src/userLogicTier/Message.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package userLogicTier;
+
+import java.io.Serializable;
+import userLogicTier.model.User;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Message implements Serializable{
+   private User User;
+   private MessageType MessageType;
+
+    public Message(User User, MessageType MessageType) {
+        this.User = User;
+        this.MessageType = MessageType;
+    }
+
+    public User getUser() {
+        return User;
+    }
+
+    public MessageType getMessageType() {
+        return MessageType;
+    }
+
+    public void setUser(User User) {
+        this.User = User;
+    }
+
+    public void setMessageType(MessageType MessageType) {
+        this.MessageType = MessageType;
+    }
+   
+   
+   
+}
---
+++ src/userLogicTier/MessageType.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package userLogicTier;
+
+/**
+ *
+ * @author Usuario
+ */
+public enum MessageType {
+    SERVER_SIGN_IN_REQUEST,
+    SERVER_SIGN_UP_REQUEST,
+    SERVER_RESPONSE_DENIED,
+    SERVER_RESPONSE_OK,
+    SERVER_MAXIMUM_USERS_REACHED,
+    SERVER_USER_ALREADY_EXIST,
+    
+}
